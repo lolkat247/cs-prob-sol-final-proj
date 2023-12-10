d8ce8d4841b653b97bdc626610a8c6681d11d9c3
Jake Iglesias <jacob.ds.iglesias.5@gmail.com>
2023-12-10 16:09:01 -0500

    Added waveform plotting and smileys

--- audio_controller.py
+++ audio_controller.py
@@ -4,32 +4,71 @@ import matplotlib.pyplot as plt
 from pydub import AudioSegment
 from tkinter import filedialog
 import os
+import numpy as np
+from scipy.io import wavfile
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, Arc
 
 class AudioController:
     def __init__(self):
         self.model = AudioModel()
         self.view = MainView(self)
-        self.wav_ = None
+        self.wav_data = None
 
     def load_audio_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav *.mp3")])
         if file_path:
             # update the file_label with the selected file name
             self.view.file_label.config(text=file_path)
+
             # load the audio file and perform any necessary processing
             audio = AudioSegment.from_file(file_path)
+
             # convert to wav
             audio.export("converted.wav", format="wav")
-            # load the wav data into memory
-            with open("converted.wav", "rb") as wav_file:
-                self.wav_data = wav_file.read()
+
+            # read the sample rate and data from the converted WAV file
+            sample_rate, data = wavfile.read("converted.wav")
+
+            # Check if the audio has two channels (stereo)
+            if data.ndim == 2:
+                # Convert stereo to mono by averaging the two channels
+                data = data.mean(axis=1)
+
+            # update the duration_label with the duration of the audio file
+            duration = len(data) / sample_rate
+            if duration: self.view.duration_label.config(text=f"Duration:\n{duration:.2f} seconds")
+
+            # update the frequency_label with the peak resonant frequency
+            frequency = self.calculate_peak_resonant_frequency()  # TODO: Implement this method
+            if frequency: self.view.frequency_label.config(text=f"Peak Resonant Frequency:\n{frequency:.2f} Hz")
+
+            # update the waveform plot
+            times = np.arange(len(data)) / float(sample_rate)
+            self.view.waveform_ax.clear()
+            self.view.waveform_ax.plot(times, data)
+            self.view.waveform_canvas.draw()
+
+            # Apply customizations to the plot
+            self.view.waveform_ax.set_title('Audio Waveform', fontsize=14, fontweight='bold')
+            self.view.waveform_ax.set_xlabel('Time (s)', fontsize=12)
+            self.view.waveform_ax.set_ylabel('Amplitude', fontsize=12)
+            self.view.waveform_ax.grid(True)
+            self.view.waveform_ax.plot(times, data, color='royalblue', linewidth=1.5)
+
+            # Use a tight layout to optimize spacing
+            self.view.waveform_fig.tight_layout()
+
+            self.view.waveform_canvas.draw()
+
             # remove the temporary file
             os.remove("converted.wav")
 
-    def clean_audio_data(self):
-        pass
-
-    def analyze_audio(self):
+    def calculate_peak_resonant_frequency(self):
         pass
 
     def combine_plots(self):
--- main_view.py
+++ main_view.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, Arc
+
 
 class MainView:
     def __init__(self, controller):
@@ -27,21 +31,105 @@ class MainView:
         self.middle_frame = tk.Frame(self.root)
         self.middle_frame.pack(fill=tk.BOTH, expand=True)
 
+        # info section
+        self.info_frame = tk.Frame(self.middle_frame, width=200)
+        self.info_frame.pack(side=tk.LEFT, fill=tk.Y)
+
+        # label for displaying audio file duration
+        self.duration_label = tk.Label(self.info_frame, text="Duration:\nN/A", justify=tk.LEFT)
+        self.duration_label.pack(anchor='w')
+
+        # label for displaying peak resonant frequency
+        self.frequency_label = tk.Label(self.info_frame, text="Peak Resonant Frequency:\nN/A", justify=tk.LEFT)
+        self.frequency_label.pack(anchor='w')
+
+        # right side frame
+        self.right_frame = tk.Frame(self.middle_frame)
+        self.right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+        # waveform plot
+        self.waveform_frame = tk.Frame(self.right_frame)
+        self.waveform_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=False)
+        self.waveform_fig = Figure(figsize=(5, 2), dpi=100)
+        self.waveform_ax = self.waveform_fig.add_subplot(111)
+        self.waveform_canvas = FigureCanvasTkAgg(self.waveform_fig, master=self.waveform_frame)
+        self.waveform_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        # Draw the initial smiley face
+        self.draw_smiley_face()
+
         # tabbed plots
-        self.tab_control = ttk.Notebook(self.middle_frame)
+        self.tab_control = ttk.Notebook(self.right_frame)
         self.tab1 = ttk.Frame(self.tab_control)
         self.tab2 = ttk.Frame(self.tab_control)
         self.tab3 = ttk.Frame(self.tab_control)
         self.tab_control.add(self.tab1, text='RT60 Low')
         self.tab_control.add(self.tab2, text='RT60 Mid')
         self.tab_control.add(self.tab3, text='RT60 High')
-        self.tab_control.pack(fill=tk.BOTH, expand=True)
+        self.tab_control.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        # button to combine plots
-        self.combine_button = tk.Button(self.middle_frame, text="Combine Plots", command=self.controller.combine_plots)
-        self.combine_button.pack(side=tk.BOTTOM)
+        # Create figures and axes for each tab
+        self.fig1, self.ax1 = plt.subplots()
+        self.fig2, self.ax2 = plt.subplots()
+        self.fig3, self.ax3 = plt.subplots()
+
+        # Draw smiley faces on each tab
+        self.draw_smiley_on_tab(self.fig1, self.ax1)
+        self.draw_smiley_on_tab(self.fig2, self.ax2)
+        self.draw_smiley_on_tab(self.fig3, self.ax3)
+
+        # Embed the figures in the tabs
+        self.canvas1 = FigureCanvasTkAgg(self.fig1, master=self.tab1)
+        self.canvas1.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.canvas2 = FigureCanvasTkAgg(self.fig2, master=self.tab2)
+        self.canvas2.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.canvas3 = FigureCanvasTkAgg(self.fig3, master=self.tab3)
+        self.canvas3.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def draw_smiley_face(self):
+        # Clear the current plot
+        self.waveform_ax.clear()
+
+        # Set the title
+        self.waveform_ax.set_title('Audio Waveform', fontsize=14, fontweight='bold')
+
+        # Draw a smiley face
+        face = Circle((0.5, 0.5), 0.4, color='yellow', fill=True)
+        left_eye = Circle((0.35, 0.65), 0.05, color='black', fill=True)
+        right_eye = Circle((0.65, 0.65), 0.05, color='black', fill=True)
+        smile = Arc((0.5, 0.4), 0.4, 0.2, angle=0, theta1=210, theta2=330, color='black', linewidth=2)
+
+        self.waveform_ax.add_patch(face)
+        self.waveform_ax.add_patch(left_eye)
+        self.waveform_ax.add_patch(right_eye)
+        self.waveform_ax.add_patch(smile)
+
+        # Hide the axes
+        self.waveform_ax.axis('off')
+
+        # Refresh the canvas
+        self.waveform_canvas.draw()
+
+    def draw_smiley_on_tab(self, fig, ax):
+        # Clear the current plot
+        ax.clear()
+
+        # Draw a smiley face
+        face = Circle((0.5, 0.5), 0.4, color='yellow', fill=True)
+        left_eye = Circle((0.35, 0.65), 0.05, color='black', fill=True)
+        right_eye = Circle((0.65, 0.65), 0.05, color='black', fill=True)
+        smile = Arc((0.5, 0.4), 0.4, 0.2, angle=0, theta1=210, theta2=330, color='black', linewidth=2)
+
+        ax.add_patch(face)
+        ax.add_patch(left_eye)
+        ax.add_patch(right_eye)
+        ax.add_patch(smile)
+
+        # Hide the axes
+        ax.axis('off')
+
+        # Refresh the canvas
+        fig.canvas.draw()
 
     def run(self):
         self.root.mainloop()
-
-    # Additional methods for updating the view, e.g., display results
